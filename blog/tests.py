16649e4d4d9920ed40def96ee1c0506d20da0cfc
Sujin Kim <sujin@swu.ac.kr>
2022-01-11 16:21:36 +0900

    categories in post_detail page

--- blog/tests.py
+++ blog/tests.py
@@ -61,6 +61,14 @@ class TestView(TestCase):
         self.assertIn('Blog', navbar.text)
         self.assertIn('About me', navbar.text)
 
+    def check_right_side(self, soup):
+        category_card = soup.find('div', id='category-card')
+
+        self.assertIn('미분류 (1)', category_card.text)  # 미분류 (1) 있어야 함
+        self.assertIn('정치/사회 (1)', category_card.text)  # 정치/사회 (1) 있어야 함
+
+
+
     def test_post_list_no_post(self):
         response = self.client.get('/blog/')
         self.assertEqual(response.status_code, 200)
@@ -102,16 +110,11 @@ class TestView(TestCase):
         post_000_read_more_btn = body.find('a', id='read-more-post-{}'.format(post_000.pk))
         self.assertEqual(post_000_read_more_btn['href'], post_000.get_absolute_url())
 
-        # category card 에서
-        category_card = body.find('div', id='category-card')
-
-        self.assertIn('미분류 (1)', category_card.text) # 미분류 (1) 있어야 함
-        self.assertIn('정치/사회 (1)', category_card.text) # 정치/사회 (1) 있어야 함
-
+        self.check_right_side(soup)
         ### main_div에는
-        main_div = body.find('div', id='main_div')
-        self.assertIn('정치/사회', main_div.text) # '정치/사회' 있어야 함
-        self.assertIn('미분류', main_div.text) # '미분류' 있어야 함
+        main_div = soup.find('div', id='main_div')
+        self.assertIn('정치/사회', main_div.text)  # '정치/사회' 있어야 함
+        self.assertIn('미분류', main_div.text)  # '미분류' 있어야 함
 
     def test_post_detail(self):
         post_000 = create_post(
@@ -120,7 +123,14 @@ class TestView(TestCase):
             author=self.author_000,
         )
 
+        post_001 = create_post(
+            title='The second post',
+            content='Second Second Second',
+            author=self.author_000,
+            category=create_category(name='정치/사회'),
+        )
         self.assertGreater(Post.objects.count(), 0)
+
         post_000_url = post_000.get_absolute_url()
         self.assertEqual(post_000_url, '/blog/{}/'.format(post_000.pk))
 
@@ -141,3 +151,5 @@ class TestView(TestCase):
         self.assertIn(post_000.author.username, main_div.text)
 
         self.assertIn(post_000.content, main_div.text)
+
+        self.check_right_side(soup)
